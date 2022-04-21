c687b0816f5f3cb0b0c0797a686ebd51d7dcdd88
wataru <wataru775@gmail.com>
2022-04-21 10:25:16 +0900

    php artisan make:seeder BooksSeeder

---
+++ database/seeders/BooksSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BooksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->insert([
+            'title' => 'ドメイン特化言語 パターンで学ぶDSLのベストプラクティス46項目',
+        ]);
+    }
+}
