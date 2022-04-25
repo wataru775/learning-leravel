4a2f33d7e861dca40ae00405ce98932b320c1ef1
wataru <wataru775@gmail.com>
2022-04-25 10:53:22 +0900

    test : サンプルデータを追加

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -15,8 +15,10 @@ class BooksSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('books')->insert([
-            'title' => 'ドメイン特化言語 パターンで学ぶDSLのベストプラクティス46項目',
-        ]);
+        DB::table('books')->insert(['title' => 'ドメイン特化言語 パターンで学ぶDSLのベストプラクティス46項目',]);
+        DB::table('books')->insert(['title' => '３月のライオン(１３)',]);
+        DB::table('books')->insert(['title' => 'ゼロから作るDeep Learning ―Pythonで学ぶディープラーニングの理論と実装',]);
+        DB::table('books')->insert(['title' => 'WORLD END ECONOMiCA (2) (電撃文庫)',]);
+        DB::table('books')->insert(['title' => 'WORLD END ECONOMiCA (3) (電撃文庫)',]);
     }
 }
