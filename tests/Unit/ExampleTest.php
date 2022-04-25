b1e312702ca8439eb224f0ccc11cc54a37fe8c7b
wataru <wataru775@gmail.com>
2022-04-25 10:39:13 +0900

    test : 標準試験に参照試験を書いてしまったのを修正

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,25 +2,12 @@
 
 namespace Tests\Unit;
 
-use App\Models\Book;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function setup(): void
+    public function test_that_true_is_true()
     {
-        parent::setUp();
-        $this->artisan('migrate');
-        $this->seed('BooksSeeder');
-    }
-
-    /**
-     * 直接DB参照できること
-     */
-    public function test_direct_access()
-    {
-        $book = Book::find(1);
-
-        $this->assertEquals('ドメイン特化言語 パターンで学ぶDSLのベストプラクティス46項目' , $book->title);
+        $this->assertTrue(true);
     }
 }
